4fd7f76f189faf2cc71fa45d015846bc225845b9
David Parra Perez <dparra0007@users.noreply.github.com>
2017-07-05 12:30:10 +0200

    Change messages

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export USERAPI_ERRMESSAGE="Sorry, but some error have happened"
-export USERAPI_ADDMESSAGE="User added"
-export USERAPI_DELMESSAGE="user deleted"
+export USERAPI_ADDMESSAGE="Content validated successfully!"
+export USERAPI_DELMESSAGE="Content not valid"
